a62c72d1ee9e3bff5ed4aba06acf4faf1bd2ccad
RicardoZombie <105948331+RicardoZombie@users.noreply.github.com>
2024-01-31 02:35:18 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,51 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
 
-# Função para o comando /start
-def start(update, context):
-    update.message.reply_text('Olá! Bem-vindo ao seu bot.')
+CHAVE_API = "botapi"
 
-# Função para o comando /echo
-def echo(update, context):
-    # Responder à mensagem de texto com a mesma mensagem
-    update.message.reply_text(update.message.text)
+bot = telebot.TeleBot(CHAVE_API)
 
-# Função principal
-def main():
-    # Substitua 'SEU_TOKEN' pelo token do seu bot Telegram
-    updater = Updater('SEU_TOKEN', use_context=True)
+@bot.message_handler(commands=["pizza"])
+def pizza(mensagem):
+    bot.send_message(mensagem.chat.id, "Saindo a pizza pra sua casa: Tempo de espera em 20min")
 
-    dp = updater.dispatcher
+@bot.message_handler(commands=["hamburguer"])
+def hamburguer(mensagem):
+    bot.send_message(mensagem.chat.id, "Saindo o Brabo: em 10min chega ai")
 
-    # Adicione manipuladores de comando
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("echo", echo))
+@bot.message_handler(commands=["salada"])
+def salada(mensagem):
+    bot.send_message(mensagem.chat.id, "Não tem salada não, clique aqui para iniciar: /iniciar")
 
-    # Adicione um manipulador para mensagens de texto
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+@bot.message_handler(commands=["opcao1"])
+def opcao1(mensagem):
+    texto = """
+    O que você quer? (Clique em uma opção)
+    /pizza Pizza
+    /hamburguer Hamburguer
+    /salada Salada"""
+    bot.send_message(mensagem.chat.id, texto)
 
-    # Inicie o bot
-    updater.start_polling()
+@bot.message_handler(commands=["opcao2"])
+def opcao2(mensagem):
+    bot.send_message(mensagem.chat.id, "Para enviar uma reclamação, mande um e-mail para reclamação@balbalba.com")
 
-    # Aguarde o bot ser encerrado com Ctrl+C
-    updater.idle()
+@bot.message_handler(commands=["opcao3"])
+def opcao3(mensagem):
+    bot.send_message(mensagem.chat.id, "Valeu! Lira mandou um abraço de volta")
 
-if __name__ == '__main__':
-    main()
+
+
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    texto = """
+    Escolha uma opção para continuar (Clique no item):
+     /opcao1 Fazer um pedido
+     /opcao2 Reclamar de um pedido
+     /opcao3 Mandar um abraço pro Lira
+Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
+    bot.reply_to(mensagem, texto)
+
+bot.polling()
